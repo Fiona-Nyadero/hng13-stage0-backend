5c0ae846fb0177d9361fb5a169b500cfe83027e6
Fiona-Nyadero <fionanyadero@gmail.com>
2025-10-19 11:04:13 +0300

    Stage 0- backend tasks

---
+++ README.md
@@ -0,0 +1,21 @@
+# HNG Backend Stage 0 — Dynamic Profile Endpoint
+
+## Description
+
+This project implements a RESTful API endpoint `/me` that returns profile information and a random cat fact fetched from an external API.
+
+## Tech Stack
+
+- Node.js
+- Express.js
+- Axios
+
+## Setup Instructions
+
+### 1. Clone this repo
+
+```bash
+git clone https://github.com/Fiona-Nyadero/hng13-stage0-backend.git
+cd hng-backend-stage0
+npm install
+```
---
+++ server.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const USER_INFO = {
+    email: 'fionanyadero@gmail.com',
+    name: 'Fiona Nyadero',
+    stack: 'Node.js/Express/Axios'
+};
+
+// API endpoints
+app.get('/me', async (req, res) => {
+    const timestamp = new Date().toISOString();
+
+    try {
+        const response = await axios.get('https://catfact.ninja/fact', {
+            timeout: 5000
+        });
+
+        const catFact = response.data.fact;
+
+        res.status(200).json({
+            status: 'success',
+            user: USER_INFO,
+            timestamp,
+            fact: catFact,
+        });
+
+    } catch (error) {
+        console.error('Error fetching cat fact:', error.message);
+
+        res.status(200).json({
+            status: 'success',
+            user: USER_INFO,
+            timestamp,
+            fact: 'Cats are mysterious, even when APIs fail',
+        });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(
+    `Server running on port ${PORT}`
+));
